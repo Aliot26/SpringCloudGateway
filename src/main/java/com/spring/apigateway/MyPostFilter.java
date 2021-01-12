6103089ad459691adf8c9d7ba87fef82a15b8b7c
Aliot26 <olgakochanowa@gmail.com>
2021-01-12 20:02:34 +0100

    added PostFilter

--- src/main/java/com/spring/apigateway/MyPostFilter.java
+++ src/main/java/com/spring/apigateway/MyPostFilter.java
@@ -1,7 +1,25 @@
 package com.spring.apigateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /*
  *Created by olga on 12.01.2021
  */
-public class MyPostFilter {
+@Component
+public class MyPostFilter implements GlobalFilter {
+
+    Logger logger = LoggerFactory.getLogger(MyPostFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Global PostFilter executed...............");
+        }));
+    }
 }
